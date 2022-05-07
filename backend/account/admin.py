fb468705bb3567212dc3ddb5dd174e4295ea4c62
sepehr <sepehrhozzari99@gmail.com>
2022-05-07 20:08:04 +0430

    add fieldsets to UserAdmin

--- backend/account/admin.py
+++ backend/account/admin.py
@@ -3,4 +3,7 @@ from .models import User
 from django.contrib.auth.admin import UserAdmin
 
 
+UserAdmin.fieldsets += (None,
+                        {"fields": ("profile_picture", "city", "address")}),
+
 admin.site.register(User, UserAdmin)
