cfcc7d146d6a36bcff823936d1532ced5c683625
sepehr <sepehrhozzari99@gmail.com>
2022-05-07 19:23:50 +0430

    initialize account app

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+
+    # our apps
+    'account.apps.AccountConfig'
 ]
 
 MIDDLEWARE = [
