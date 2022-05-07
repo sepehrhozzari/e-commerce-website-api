0a688f7e667e07455975cc58f74728d151340872
sepehr <sepehrhozzari99@gmail.com>
2022-05-07 20:07:48 +0430

    add blank=True to city and address field

--- backend/account/models.py
+++ backend/account/models.py
@@ -14,5 +14,5 @@ class User(AbstractUser):
     email = models.EmailField(unique=True, verbose_name="ایمیل")
     profile_picture = models.ImageField(upload_to="account/", blank=True)
     city = models.CharField(
-        max_length=20, choices=CITY_CHOICES, verbose_name="شهر")
-    address = models.TextField(verbose_name="آدرس")
+        max_length=20, choices=CITY_CHOICES, blank=True, verbose_name="شهر")
+    address = models.TextField(blank=True, verbose_name="آدرس")
