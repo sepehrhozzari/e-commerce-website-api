9f4fd95a0bb56efb76e0fdacf27904363d4914cd
sepehr <sepehrhozzari99@gmail.com>
2022-05-07 20:10:50 +0430

    add city field to UserAdmin list_display

--- backend/account/admin.py
+++ backend/account/admin.py
@@ -5,5 +5,6 @@ from django.contrib.auth.admin import UserAdmin
 
 UserAdmin.fieldsets += (None,
                         {"fields": ("profile_picture", "city", "address")}),
+UserAdmin.list_display += ("city",)
 
 admin.site.register(User, UserAdmin)
