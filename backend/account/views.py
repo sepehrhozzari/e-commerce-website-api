381aa855ddb01c1c6cd12463d27dc1077a687231
sepehr <sepehrhozzari99@gmail.com>
2022-05-07 21:04:25 +0430

    add IsSuperUserOrAdmin permission to UserList

--- backend/account/views.py
+++ backend/account/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 from rest_framework.generics import ListAPIView, RetrieveAPIView
 from .models import User
 from .serializers import UserSerializer
+from .permissions import IsSuperUserOrAdmin
 
 
 class UserList(ListAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = [IsSuperUserOrAdmin, ]
