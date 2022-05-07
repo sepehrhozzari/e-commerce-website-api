4d062affdf956ea7596915380dd9f1ccd0aabe59
sepehr <sepehrhozzari99@gmail.com>
2022-05-07 19:58:40 +0430

    change language to fa-ir

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -112,7 +112,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fa-ir'
 
 TIME_ZONE = 'UTC'
 
