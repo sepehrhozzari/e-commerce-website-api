b89c3e4a24c35f14f97c408f835d3c660a67ee3a
sepehr <sepehrhozzari99@gmail.com>
2022-05-07 21:12:12 +0430

    create url for UserRetrieve

--- backend/account/urls.py
+++ backend/account/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import UserList
+from .views import UserList, UserRetrieve
 
 
 urlpatterns = [
     path('users/', UserList.as_view(), name="users-list"),
+    path('users/<int:pk>/', UserRetrieve.as_view(), name="users-detail"),
 ]
