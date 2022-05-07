6eb2736627235d9042f3e874dfec598ef43f8a9b
sepehr <sepehrhozzari99@gmail.com>
2022-05-07 19:34:45 +0430

    create User model

--- backend/account/models.py
+++ backend/account/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    CITY_CHOICES = (
+        ("TEHRAN", "تهران"),
+        ("SHIRAZ", "شیراز"),
+        ("ISFAHAN", "اصفحان"),
+        ("ARDABIL", "اردبیل"),
+        ("TABRIZ", "تبریز"),
+        ("KARAJ", "کرج"),
+    )
+    email = models.EmailField(unique=True, verbose_name="ایمیل")
+    profile_picture = models.ImageField(upload_to="account/", blank=True)
+    city = models.CharField(
+        max_length=20, choices=CITY_CHOICES, verbose_name="شهر")
+    address = models.TextField(verbose_name="آدرس")
