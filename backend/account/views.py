61c5c8c6816b62720b240083f2ccdf3ab7080630
sepehr <sepehrhozzari99@gmail.com>
2022-05-07 20:53:41 +0430

    create UserListAPIView

--- backend/account/views.py
+++ backend/account/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework.generics import ListAPIView, RetrieveAPIView
+from .models import User
+from .serializers import UserSerializer
 
-# Create your views here.
+
+class UserList(ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
