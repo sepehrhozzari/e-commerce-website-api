0be00a97a70fa4a62e456536a6dbd56df47fe6dc
sepehr <sepehrhozzari99@gmail.com>
2022-05-09 19:50:21 +0430

    create Item model

--- backend/cart/models.py
+++ backend/cart/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from account.models import User
 
-# Create your models here.
+
+class Item(models.Model):
+    title = models.CharField(max_length=200, verbose_name="عنوان محصول")
+    image = models.ImageField(upload_to="cart/", verbose_name="تصویر محصول")
+    price = models.DecimalField(
+        max_digits=10, decimal_places=2, verbose_name="قیمت محصول")
+    discount_price = models.DecimalField(
+        max_digits=10, decimal_places=2, verbose_name="قیمت محصول با تخفیف")
+    description = models.TextField(verbose_name="توضیحات محصول")
+    in_stock = models.IntegerField(default=True, verbose_name="موجود در انبار")
+    likes = models.ManyToManyField(
+        User, blank=True, related_name="liked_items", verbose_name="لایک")
+    dislikes = models.ManyToManyField(
+        User, blank=True, related_name="disliked_items", verbose_name="دیس لایک")
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
