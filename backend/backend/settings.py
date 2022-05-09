a1f9897c90798ea6081353f1191d9fce59021962
sepehr <sepehrhozzari99@gmail.com>
2022-05-09 23:25:39 +0430

    add DEFAULT_PAGINATION_CLASS to settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -156,6 +156,8 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "dj_rest_auth.jwt_auth.JWTCookieAuthentication",
     ],
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 30
 }
 REST_USE_JWT = True
 JWT_AUTH_COOKIE = 'access'
