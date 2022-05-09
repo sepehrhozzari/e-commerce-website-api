5dddd7db2b1de40e2847d9b1bb14ec39ffe1fc0d
sepehr <sepehrhozzari99@gmail.com>
2022-05-10 00:31:39 +0430

    add permission_classes to ItemViewSet

--- backend/cart/views.py
+++ backend/cart/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 from .models import Item
 from .serializers import ItemDisplaySerializer, ItemSerializer
+from account.permissions import IsAdminOrReadOnly
 
 
 class ItemViewSet(ModelViewSet):
     queryset = Item.objects.in_stock().prefetch_related("likes", "dislikes", "hits")
+    permission_classes = [IsAdminOrReadOnly, ]
 
     def get_serializer_class(self):
         if self.action in ["list", "retrieve"]:
