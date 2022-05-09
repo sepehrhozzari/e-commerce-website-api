8565c2af0248ee2e7baca1d07f4c42ec81bb65b8
sepehr <sepehrhozzari99@gmail.com>
2022-05-09 21:34:13 +0430

    create ItemViewSet

--- backend/cart/views.py
+++ backend/cart/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .models import Item
+from .serializers import ItemDisplaySerializer, ItemSerializer
 
-# Create your views here.
+
+class ItemViewSet(ModelViewSet):
+    queryset = Item.objects.in_stock().prefetch_related("likes", "dislikes", "hits")
+
+    def get_serializer_class(self):
+        if self.action in ["list", "retrieve"]:
+            return ItemDisplaySerializer
+        return ItemSerializer
