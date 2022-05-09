22c06b82841035da2640c18bf55c6ed4e247a5ae
sepehr <sepehrhozzari99@gmail.com>
2022-05-09 20:18:22 +0430

    create IPAddress model

--- backend/account/models.py
+++ backend/account/models.py
@@ -16,3 +16,7 @@ class User(AbstractUser):
     city = models.CharField(
         max_length=20, choices=CITY_CHOICES, blank=True, verbose_name="شهر")
     address = models.TextField(blank=True, verbose_name="آدرس")
+
+
+class IPAddress(models.Model):
+    ip_address = models.GenericIPAddressField(verbose_name="آدرس آی‌پی")
