6812155c545d633d91a8259de7a640a39726f180
sepehr <sepehrhozzari99@gmail.com>
2022-05-10 00:56:20 +0430

    add list_filter to the ItemAdmin

--- backend/cart/admin.py
+++ backend/cart/admin.py
@@ -5,6 +5,7 @@ from .models import Item
 class ItemAdmin(admin.ModelAdmin):
     list_display = ("title", "price", "discount_price", "image_tag",
                     "in_stock", "like_count", "dislike_count", "hits_count")
+    list_filter = ("in_stock",)
 
 
 admin.site.register(Item, ItemAdmin)
