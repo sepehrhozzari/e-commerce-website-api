200f181df4cbea5dae8babda00f781cf735375ea
sepehr <sepehrhozzari99@gmail.com>
2022-05-09 23:17:00 +0430

    rename IsSuperUserOrAdmin permission to IsAdmin

--- backend/account/permissions.py
+++ backend/account/permissions.py
@@ -1,7 +1,7 @@
 from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 
-class IsSuperUserOrAdmin(BasePermission):
+class IsAdmin(BasePermission):
     def has_permission(self, request, view):
         return bool(
             request.user.is_authenticated and request.user.is_superuser or
--- backend/account/views.py
+++ backend/account/views.py
@@ -2,20 +2,20 @@ from django.shortcuts import render
 from rest_framework.generics import ListAPIView, RetrieveAPIView
 from .models import User
 from .serializers import UserSerializer, CustomizedUserDetailSerializer
-from .permissions import IsSuperUserOrAdmin
+from .permissions import IsAdmin
 from dj_rest_auth.views import UserDetailsView
 
 
 class UserList(ListAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = [IsSuperUserOrAdmin, ]
+    permission_classes = [IsAdmin, ]
 
 
 class UserRetrieve(RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = [IsSuperUserOrAdmin, ]
+    permission_classes = [IsAdmin, ]
 
 
 class CustomizedUserDetailsView(UserDetailsView):
