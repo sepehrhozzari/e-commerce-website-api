1c65c3fd1fa437eefb189284df5fffc433912cb6
sepehr <sepehrhozzari99@gmail.com>
2022-05-09 23:02:40 +0430

    add request.user.is_authenticated

--- backend/account/permissions.py
+++ backend/account/permissions.py
@@ -5,5 +5,5 @@ class IsSuperUserOrAdmin(BasePermission):
     def has_permission(self, request, view):
         return bool(
             request.user.is_authenticated and request.user.is_superuser or
-            request.user.is_staff
+            request.user.is_authenticated and request.user.is_staff
         )
