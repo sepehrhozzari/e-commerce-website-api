1139d7c496c8dafef10e954a5a153857e2a6a0b6
sepehr <sepehrhozzari99@gmail.com>
2022-05-09 21:47:20 +0430

    create Category model

--- backend/category/models.py
+++ backend/category/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    parent = models.ForeignKey("self", null=True, on_delete=models.SET_NULL,
+                               related_name="children", verbose_name="زیردسته")
+    title = models.CharField(max_length=200, verbose_name="عنوان دسته بندی")
+    image = models.ImageField(upload_to="category/",
+                              verbose_name="تصویر دسته بندی")
+    is_active = models.BooleanField(default=True, verbose_name="فعال")
+    position = models.IntegerField(verbose_name="پوزیشن")
