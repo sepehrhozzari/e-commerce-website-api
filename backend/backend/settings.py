67cf4faa32b1772efb0303922ab25200ce34b059
sepehr <sepehrhozzari99@gmail.com>
2022-05-09 23:37:17 +0430

    add DEFAULT_PERMISSION_CLASSES

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -157,6 +157,9 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "dj_rest_auth.jwt_auth.JWTCookieAuthentication",
     ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permission.IsAuthenticatedOrReadOnly",
+    ],
     "DEFAULT_FILTER_BACKENDS": [
         "django_filters.rest_framework.DjangoFilterBackend",
         "rest_framework.filters.SearchFilter",
