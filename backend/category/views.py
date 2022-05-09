a23f1b80ec67a51d7aa394a77e5fba8774bf0b7b
sepehr <sepehrhozzari99@gmail.com>
2022-05-09 22:49:43 +0430

    create CategoryViewSet

--- backend/category/views.py
+++ backend/category/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .models import Category
+from .serializers import CategoryDisplaySerializer, CategorySerializer
 
-# Create your views here.
+
+class CategoryViewSet(ModelViewSet):
+    queryset = Category.objects.is_active().select_related("parent")
+
+    def get_serializer_class(self):
+        if self.action in ["list", "retrieve"]:
+            return CategoryDisplaySerializer
+        return CategorySerializer
