f1f18a2a2220b60e07f97775db96cdac37fca5a0
sepehr <sepehrhozzari99@gmail.com>
2022-05-09 22:39:11 +0430

    add search fields to CategoryAdmin

--- backend/category/admin.py
+++ backend/category/admin.py
@@ -5,6 +5,7 @@ from .models import Category
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ("title", "image_tag", "is_active", "position", "parent")
     list_filter = ("is_active", "position", "parent")
+    search_fields = ("title",)
 
 
 admin.site.register(Category, CategoryAdmin)
