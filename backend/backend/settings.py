2559a00aec2c9d12d0c2148b3f3b64954ba7789a
sepehr <sepehrhozzari99@gmail.com>
2022-05-09 23:35:11 +0430

    add OrderingFilter to default filter backends

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -160,6 +160,7 @@ REST_FRAMEWORK = {
     "DEFAULT_FILTER_BACKENDS": [
         "django_filters.rest_framework.DjangoFilterBackend",
         "rest_framework.filters.SearchFilter",
+        "rest_framework.filters.OrderingFilter",
     ],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 30
