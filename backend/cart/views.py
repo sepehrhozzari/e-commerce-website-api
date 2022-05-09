70e83658fd5f8b19d4e69a105d319d3ef1ffad62
sepehr <sepehrhozzari99@gmail.com>
2022-05-10 00:35:01 +0430

    add filterset_fields to ItemViewSet

--- backend/cart/views.py
+++ backend/cart/views.py
@@ -6,8 +6,9 @@ from account.permissions import IsAdminOrReadOnly
 
 
 class ItemViewSet(ModelViewSet):
-    queryset = Item.objects.in_stock().prefetch_related("likes", "dislikes", "hits")
+    queryset = Item.objects.prefetch_related("likes", "dislikes", "hits")
     permission_classes = [IsAdminOrReadOnly, ]
+    filterset_fields = ("in_stock",)
 
     def get_serializer_class(self):
         if self.action in ["list", "retrieve"]:
