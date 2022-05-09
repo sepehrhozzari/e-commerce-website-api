410fe26f30a1ea06228e628ecb729fe232f34b8e
sepehr <sepehrhozzari99@gmail.com>
2022-05-10 00:43:48 +0430

    create ItemAdmin

--- backend/cart/admin.py
+++ backend/cart/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 from .models import Item
 
 
-admin.site.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ("title", "price", "discount_price", "image_tag",
+                    "in_stock", "like_count", "dislike_count", "hits_count")
+
+
+admin.site.register(Item, ItemAdmin)
