7f8bf0f3f78d903a2284ef085bb82cfa3b45c3f2
sepehr <sepehrhozzari99@gmail.com>
2022-05-09 23:17:25 +0430

    add permission_classes to CategoryViewSet

--- backend/category/views.py
+++ backend/category/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 from .models import Category
+from account.permissions import IsAdminOrReadOnly
 from .serializers import CategoryDisplaySerializer, CategorySerializer
 
 
 class CategoryViewSet(ModelViewSet):
     queryset = Category.objects.is_active().select_related("parent")
+    permission_classes = [IsAdminOrReadOnly, ]
 
     def get_serializer_class(self):
         if self.action in ["list", "retrieve"]:
