245b52bb107023ec1096802a2d5bc5e2959ce085
sepehr <sepehrhozzari99@gmail.com>
2022-05-11 22:11:37 +0430

    admin register for the CartItem model

--- backend/cart/admin.py
+++ backend/cart/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Item
+from .models import Item, CartItem
 
 
 class ItemAdmin(admin.ModelAdmin):
@@ -10,3 +10,4 @@ class ItemAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Item, ItemAdmin)
+admin.site.register(CartItem)
