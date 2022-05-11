fa84b8bdf0a5357b2e4ce91e80ca6e021c22d823
sepehr <sepehrhozzari99@gmail.com>
2022-05-12 00:18:42 +0430

    add get_total_price to the Cart model

--- backend/cart/models.py
+++ backend/cart/models.py
@@ -110,3 +110,9 @@ class Cart(models.Model):
         verbose_name = "سبد خرید"
         verbose_name_plural = "سبد های خرید"
         ordering = ("-is_paid",)
+
+    def get_total_price(self):
+        total = 0
+        for cart_item in self.items.all():
+            total += cart_item.get_total_price()
+        return total
