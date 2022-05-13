29478cf0f92361e13c49fe78134770794f84bb70
sepehr <sepehrhozzari99@gmail.com>
2022-05-13 14:32:50 +0430

    create router for CartItemViewSet

--- backend/cart/urls.py
+++ backend/cart/urls.py
@@ -1,9 +1,10 @@
 from rest_framework import routers
-from .views import ItemViewSet
+from .views import ItemViewSet, CartItemViewSet
 
 
 router = routers.SimpleRouter()
 router.register("items", ItemViewSet, basename="items")
+router.register("cart-items", CartItemViewSet, basename="cart_items")
 
 
 urlpatterns = router.urls
