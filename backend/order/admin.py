8daf3f88584fd65380cd41c3a969f22ac9af245a
sepehr <sepehrhozzari99@gmail.com>
2022-05-14 23:24:45 +0430

    add search_fields to the OrderItemAdmin

--- backend/order/admin.py
+++ backend/order/admin.py
@@ -4,6 +4,8 @@ from .models import OrderItem
 
 class OrderItemAdmin(admin.ModelAdmin):
     list_display = ("user", "item", "quantity")
+    search_fields = ("item__title", "item__description",
+                     "user__username", "user__first_name", "user__last_name")
 
 
 admin.site.register(OrderItem, OrderItemAdmin)
