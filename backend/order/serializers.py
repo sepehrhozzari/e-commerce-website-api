9548e997af3b06063e7aa11b8467a701ac326234
sepehr <sepehrhozzari99@gmail.com>
2022-05-14 23:30:11 +0430

    add customized user field for OrderItemSerializer

--- backend/order/serializers.py
+++ backend/order/serializers.py
@@ -3,6 +3,8 @@ from .models import OrderItem
 
 
 class OrderItemSerializer(serializers.ModelSerializer):
+    user = serializers.CharField(source="user.username")
+
     class Meta:
         model = OrderItem
         fields = ("user", "item", "quantity", "total_price",
