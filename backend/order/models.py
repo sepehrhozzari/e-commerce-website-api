00c1f778f0af5bb61f41c0226f65b3bfd9ec2d7a
sepehr <sepehrhozzari99@gmail.com>
2022-05-14 22:03:05 +0430

    create OrderItem model

--- backend/order/models.py
+++ backend/order/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from account.models import User
+from cart.models import Item
 
-# Create your models here.
+
+class OrderItem(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE,
+                             related_name="order_items", verbose_name="کاربر")
+    item = models.ForeignKey(Item, on_delete=models.CASCADE,
+                             related_name="order_items", verbose_name="محصول")
+    quantity = models.IntegerField(default=1, verbose_name="تعداد")
