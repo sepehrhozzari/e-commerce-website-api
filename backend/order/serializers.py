fcf33a808c164218bafc9860bcc74fd9546d3d8d
sepehr <sepehrhozzari99@gmail.com>
2022-05-15 16:58:13 +0430

    create OrderSerializer

--- backend/order/serializers.py
+++ backend/order/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import OrderItem
+from .models import OrderItem, Order
 from cart.serializers import BasicItemSerializer
 
 
@@ -10,4 +10,11 @@ class OrderItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = OrderItem
         fields = ("user", "item", "quantity", "total_price",
+                  "total_discount_price", "total_amount_saved")
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        fields = ("user", "status", "items", "total_price",
                   "total_discount_price", "total_amount_saved")
